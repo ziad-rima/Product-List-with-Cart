dd1eb1e2b2764d35551be9382a3e2efe2cde0ff1
Ziad Rima <ziadrima49@gmail.com>
2025-02-16 11:39:01 +0100

    modify handleSelectButton function to handle items count

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import { useState } from "react";
 const App = () => {
   const [cartItems, setCartItems] = useState([]);
   const handleSelectButton = (item) => {
-    setCartItems(prevCartItems => [...prevCartItems, item])
+    setCartItems((prevCartItems) => {
+      let existingItem = prevCartItems.find(element => element.name === item.name)
+        if (existingItem) {
+            return prevCartItems.map(element => 
+              element.name === item.name
+              ? {...element, count: element.count++}
+              : element
+            )
+        } else {
+            return [...prevCartItems, {...item, count: 1}];
+        }
+    });
   }
   return (
     <div className='main-component'>
-      <Desserts handleSelectButton={handleSelectButton}/>
-      {cartItems.length === 0 ? <Cart /> : <FilledCart items={cartItems}/> }
+      <Desserts handleSelectButton={handleSelectButton} items={cartItems}/>
+      {cartItems.length === 0 ? <Cart /> : <FilledCart items={cartItems} />}
     </div>
   )
 }
